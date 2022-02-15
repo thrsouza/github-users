fa2661bdd05d3066233b3412559a42b94752e237
Thiago Souza <dev@thiagosouza.com>
2022-02-15 12:40:44 -0300

    Update rollup.config.prod.js

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,17 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import analyze from 'rollup-plugin-analyzer';
-import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import browsersync from 'rollup-plugin-browsersync';
 import copy from 'rollup-plugin-copy';
 import commonjs from 'rollup-plugin-commonjs';
-import filesize from 'rollup-plugin-filesize';
 import html from 'rollup-plugin-html-scaffold';
 import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
-import url from 'postcss-url';
 import progress from 'rollup-plugin-progress';
 import replace from 'rollup-plugin-replace';
 import { string } from 'rollup-plugin-string';
+import url from 'postcss-url';
 import { version } from './package.json';
 
 const appBundle = `app.es5.${version}.${new Date().getTime()}.js`;
@@ -22,7 +19,7 @@ export default [
     output: {
       file: `dist/${appBundle}`,
       format: 'umd',
-      sourcemap: false,
+      sourcemap: true,
     },
     plugins: [
       progress(),
@@ -32,7 +29,7 @@ export default [
         template: { appBundle },
       }),
       replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
+        'process.env.NODE_ENV': JSON.stringify('development'),
       }),
       string({ include: '**/*.html' }),
       postcss({
@@ -41,16 +38,12 @@ export default [
       }),
       resolve(),
       commonjs(),
-      babel(),
-      uglify(),
-      analyze(),
-      filesize(),
       copy({
         targets: {
           'public/assets': 'dist/assets',
           'public/favicon.ico': 'dist/favicon.ico',
         },
-      }),
+      }),,
     ],
   },
 ];
